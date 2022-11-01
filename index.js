4665157f7881b7101935902beae90a6e013dc2cd
Alexander Wang <zxcvb22217@gmail.com>
2022-11-01 12:50:23 +0800

    fix "[[test]]: test" not tokenized

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const { utils: { escapeHtml } } = require('remarkable')
 
 function wikilinkRule(state, silent) {
-  const {pos: start, src, posMax} = state
-  const ch = src.charCodeAt(start)
-  if (ch !== 0x5b /* [ */) return false
-  if (start + 4 >= posMax) return false
-  if (src.charCodeAt(start + 1) !== 0x5b) return false
+  const { pos, src, posMax } = state
+  let labelStart = pos
+  let labelEnd = pos
 
-  const labelStart = start + 2
-  let labelEnd = start + 2
-  state.pos = start + 2
+  // Remarkable sometimes skips the first [, so we need to look before
+  // current pos.
+  if (
+    src.charCodeAt(pos - 1) === 0x5b /* [ */ &&
+    src.charCodeAt(pos) === 0x5b /* [ */
+  ) {
+    if (pos + 3 >= posMax) return false
+    labelStart = labelEnd = pos + 1
+    state.pos = pos + 1
+  } else if (
+    src.charCodeAt(pos) === 0x5b /* [ */ &&
+    src.charCodeAt(pos + 1) === 0x5b /* [ */
+  ) {
+    if (pos + 4 >= posMax) return false
+    labelStart = labelEnd = pos + 2
+    state.pos = pos + 2
+  } else {
+    return false
+  }
+
   let found = false
   while (state.pos + 1 < posMax) {
     if (src.charCodeAt(state.pos) === 0x5d /* ] */) {
@@ -23,14 +38,18 @@ function wikilinkRule(state, silent) {
   }
 
   if (!found) {
-    state.pos = start
+    state.pos = pos
     return false
   }
 
   state.posMax = state.pos
-  state.pos = start + 2
+  state.pos = pos + 2
   if (!silent) {
-    state.push({ type: 'wikilink_open', href: src.substring(labelStart, labelEnd), level: state.level++ })
+    state.push({
+      type: 'wikilink_open',
+      href: src.substring(labelStart, labelEnd),
+      level: state.level++,
+    })
     state.linkLevel++
     state.parser.tokenize(state)
     state.linkLevel--
